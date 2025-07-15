0d82869b48454419c897085b7882447a6c599018
Chris Towles <Chris.Towles@gmail.com>
2025-07-15 01:10:04 -0400

    chore: add @antfu/ni dev dependency

--- src/utils/exec.ts
+++ src/utils/exec.ts
@@ -1,5 +1,6 @@
 import { execSync } from 'node:child_process'
 
+// TODO change to use tinyexec or similar for better error handling
 export function execCommand(cmd: string, cwd?: string) {
   // Note about execSync, if the command fails or times out, it might not throw an error,
   // if the child process intercepts the SIGTERM signal, we might not get an error.
