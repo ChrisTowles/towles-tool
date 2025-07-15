11bb5c9ae08739616a16a8fb98a4d4bec0e93fcd
Chris Towles <Chris.Towles@gmail.com>
2025-07-15 01:16:37 -0400

    it pchore: standardize git diff flag usage

--- src/utils/git.ts
+++ src/utils/git.ts
@@ -7,7 +7,8 @@ export async function getGitDiff(cwd: string): Promise<string> {
   // https://git-scm.com/docs/pretty-formats
   const r = execCommand(
     // `git --no-pager log "${from ? `${from}...` : ''}${to}"  `,
-    `git --no-pager diff --staged`,
+    // using --cached because staged didn't work on MacOS
+    `git --no-pager diff --cached`,
     // --name-status
     // --pretty="----%n%s|%h|%an|%ae%n%b"
     cwd,
