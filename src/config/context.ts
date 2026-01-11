95f05f837ed2de867980d51c9b670f745a699f13
Chris Towles <Chris.Towles@gmail.com>
2026-01-11 13:14:44 -0500

    chore: remove unused loadTowlesToolContext function
    Remove unused function from src/config/context.ts.
    Keep Context interface which is still used.

--- src/config/context.ts
+++ src/config/context.ts
@@ -1,27 +1,7 @@
-
 import type { SettingsFile } from './settings'
 
-
-// Note: the `Config` interface is used to define common configuration properties that are pased as context to various components and commands.
-  
 export interface Context {
   cwd: string
   settingsFile: SettingsFile
   debug: boolean
-}
-export async function loadTowlesToolContext({
-  cwd,
-  settingsFile,
-  debug = false
-}: {
-  cwd: string
-  settingsFile: SettingsFile
-  debug: boolean
-}): Promise<Context> {
-
-  return {
-    cwd,
-    settingsFile,
-    debug,
-  } satisfies Context
 }
