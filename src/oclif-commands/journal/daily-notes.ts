3019683538c59e2651eacb47a2a5f9d85c23a4a5
Chris Towles <Chris.Towles@gmail.com>
2026-01-11 10:18:37 -0500

    feat(oclif): migrate journal command with daily-notes, meeting, note subcommands

---
+++ src/oclif-commands/journal/daily-notes.ts
@@ -0,0 +1,31 @@
+import { BaseCommand } from '../../commands/base.js'
+import { createJournalFile } from '../../commands/journal.js'
+import { JOURNAL_TYPES } from '../../utils/parseArgs.js'
+
+/**
+ * Create or open daily notes journal file
+ */
+export default class DailyNotes extends BaseCommand {
+  static override description = 'Weekly files with daily sections for ongoing work and notes'
+
+  static override aliases = ['journal:today']
+
+  static override examples = [
+    '<%= config.bin %> journal daily-notes',
+    '<%= config.bin %> journal today',
+  ]
+
+  async run(): Promise<void> {
+    await this.parse(DailyNotes)
+
+    await createJournalFile({
+      context: {
+        cwd: process.cwd(),
+        settingsFile: this.settings.settingsFile,
+        debug: false,
+      },
+      type: JOURNAL_TYPES.DAILY_NOTES,
+      title: '',
+    })
+  }
+}
